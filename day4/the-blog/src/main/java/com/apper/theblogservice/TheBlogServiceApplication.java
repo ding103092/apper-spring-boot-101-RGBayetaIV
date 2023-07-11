33ea5170945c1802c3ad8507eea79f68238a54db
dingkun <reginald.bayeta@gmail.com>
2023-07-11 23:03:56 +0800

    Added Day 4 Files

---
+++ day4/the-blog/src/main/java/com/apper/theblogservice/TheBlogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.apper.theblogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheBlogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheBlogServiceApplication.class, args);
+	}
+
+}
